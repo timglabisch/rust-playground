a53c544515f8d74a50e96f60cf9870079cb35b08
Rémy Rakic <remy.rakic@gmail.com>
2018-03-30 00:09:30 +0200

    Add backtrace toggle to the advanced flags popover

--- ui/frontend/Header.tsx
+++ ui/frontend/Header.tsx
@@ -368,6 +368,19 @@ const ModeChannelMenu = () => (
 
 const AdvancedOptionsMenu = () => (
   <React.Fragment>
+    <PopoverGroup title="Advanced options">
+      <div className="popover-item advanced-options-item advanced-options-toggle">
+        <div className="popover-item__header">
+          <span className="popover-item__name">Backtraces</span>
+          <div className="popover-toggle">
+            <input id="backtraces-on" name="backtraces" value="on" type="radio" />
+            <label htmlFor="backtraces-on">On</label>
+            <input id="backtraces-off" name="backtraces" value="off" type="radio" checked />
+            <label htmlFor="backtraces-off">Off</label>
+          </div>
+        </div>
+      </div>
+    </PopoverGroup>
     <PopoverGroup title="Advanced options &mdash; All of them!">
       <PopoverItem name="" className="advanced-options-item">
         <input type="text" placeholder="-C or -Z ..." />
