536a9008602c819f8b84ae98354918ce0c5d7236
Jake Goulding <jake.goulding@integer32.com>
2018-03-29 12:58:22 -0400

    Close the PopButton on clicks outside of it
    This streamlines the interaction of "no, please go away popup".

--- ui/frontend/PopButton.tsx
+++ ui/frontend/PopButton.tsx
@@ -2,29 +2,38 @@ import React from 'react';
 import { Portal } from 'react-portal';
 import { Arrow, Manager, Popper, Target } from 'react-popper';
 
+type SetRefFunc = (instance: HTMLElement) => void;
+
 interface PopButtonStatelessProps {
   text: string;
   isOpen: boolean;
   onClick: () => any;
+  setPopperRef: SetRefFunc;
 }
 
 const PopButtonStateless: React.SFC<PopButtonStatelessProps> =
-  ({ text, children, isOpen, onClick }) => (
+  ({ text, children, isOpen, onClick, setPopperRef }) => (
     <Manager tag={false}>
       <Target>{({ targetProps }) => (
         <button onClick={onClick} {...targetProps}>{text}</button>
       )}</Target>
-      {isOpen && <PopButtonPopper>{children}</PopButtonPopper>}
+      {isOpen && <PopButtonPopper setPopperRef={setPopperRef}>{children}</PopButtonPopper>}
     </Manager>
   );
 
-const PopButtonPopper = ({ children }) => (
+interface PopButtonPopperProps {
+  setPopperRef: SetRefFunc;
+}
+
+const PopButtonPopper: React.SFC<PopButtonPopperProps> = ({ setPopperRef, children }) => (
   <Portal>
-    <Popper className="popper" placement="bottom">
-      <Arrow className="popper__arrow" />
-      <div className="popper__content">{children}</div>
-    </Popper>
-  </Portal >
+    <div ref={setPopperRef}>
+      <Popper className="popper" placement="bottom">
+        <Arrow className="popper__arrow" />
+        <div className="popper__content">{children}</div>
+      </Popper>
+    </div>
+  </Portal>
 );
 
 interface PopButtonProps {
@@ -41,6 +50,8 @@ export interface PopButtonEnhancements {
 }
 
 class PopButton extends React.Component<PopButtonProps, PopButtonState> {
+  private wrapperRef;
+
   constructor(props) {
     super(props);
     this.state = {
@@ -54,6 +65,24 @@ class PopButton extends React.Component<PopButtonProps, PopButtonState> {
 
   private close = () => {
     this.setState({ isOpen: false });
+  }
+
+  private setPopperRef = (r) => {
+    this.wrapperRef = r;
+  }
+
+  private handleClickOutside = () => {
+    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+      this.close()
+    }
+  }
+
+  public componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
+
+  public componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
   }
 
   public render() {
@@ -70,7 +99,8 @@ class PopButton extends React.Component<PopButtonProps, PopButtonState> {
       <PopButtonStateless
         text={text}
         isOpen={isOpen}
-        onClick={this.handleToggleVisibility}>
+        onClick={this.handleToggleVisibility}
+        setPopperRef={this.setPopperRef}>
         {enhancedChildren}
       </PopButtonStateless>
     );
