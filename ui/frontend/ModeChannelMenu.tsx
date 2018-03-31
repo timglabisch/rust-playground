b893c496bf7f45759ac1591d64c2fb7255d7dd4a
Jake Goulding <jake.goulding@integer32.com>
2018-03-31 14:00:52 -0400

    The mode menu

--- ui/frontend/ModeChannelMenu.tsx
+++ ui/frontend/ModeChannelMenu.tsx
@@ -1,32 +1,39 @@
 import React, { Fragment } from 'react';
 
 import MenuGroup from './MenuGroup';
-import SelectablePopoverItem from './SelectablePopoverItem';
+import SelectableMenuItem from './SelectableMenuItem';
 import ValidateOptions from './ValidateOptions';
 
 const ModeChannelMenu = () => (
   <Fragment>
     <MenuGroup title="Mode &mdash; Choose optimization level">
-      <SelectablePopoverItem name="Debug" selected={true}>
+      <SelectableMenuItem name="Debug" selected={true}>
         Build with debug information, without optimizations.
-      </SelectablePopoverItem>
-      <SelectablePopoverItem name="Release" selected={false}>
+      </SelectableMenuItem>
+      <SelectableMenuItem name="Release" selected={false}>
         Build with optimizations turned on.
-      </SelectablePopoverItem>
+      </SelectableMenuItem>
     </MenuGroup>
     <MenuGroup title="Channel &mdash; Choose the rust version">
-      <SelectablePopoverItem name="Stable channel" selected={true}>
-        Build using the Stable version: 1.24.1 (2018-02-05).
-      </SelectablePopoverItem>
-      <SelectablePopoverItem name="Beta channel" selected={false}>
-        Build using the Beta version: 1.25.0-beta.9 (2018-03-09).
-      </SelectablePopoverItem>
-      <SelectablePopoverItem name="Nightly channel" selected={false}>
-        Build using the Nightly version: 1.26.0-nightly (2018-03-06).
-      </SelectablePopoverItem>
+      <SelectableMenuItem name="Stable channel" selected={true}>
+        <Desc>Build using the Stable version:</Desc>
+        <Desc>1.24.1 (2018-02-05)</Desc>
+      </SelectableMenuItem>
+      <SelectableMenuItem name="Beta channel" selected={false}>
+        <Desc>Build using the Beta version:</Desc>
+        <Desc>1.25.0-beta.9 (2018-03-09)</Desc>
+      </SelectableMenuItem>
+      <SelectableMenuItem name="Nightly channel" selected={false}>
+        <Desc>Build using the Nightly version:</Desc>
+        <Desc>1.26.0-nightly (2018-03-06)</Desc>
+      </SelectableMenuItem>
     </MenuGroup>
     <ValidateOptions>Set these options</ValidateOptions>
   </Fragment>
+);
+
+const Desc: React.SFC<{}> = ({ children }) => (
+  <p className="mode-channel-menu__description">{children}</p>
 );
 
 export default ModeChannelMenu;
---
+++ ui/frontend/SelectableMenuItem.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import MenuItem from './MenuItem';
+
+interface SelectableMenuItemProps {
+  name: string;
+  selected: boolean;
+}
+
+const SelectableMenuItem: React.SFC<SelectableMenuItemProps> = ({ name, selected, children }) => (
+  <MenuItem>
+    <button className={`selectable-item ${selected ? "selectable-item--selected" : ""}`}>
+      <div className="selectable-item__header">
+        <span className="selectable-item__name">{name}</span>
+        <span className="selectable-item__checkmark">
+          <Icon />
+        </span>
+      </div>
+      <span className="selectable-item__description">{children}</span>
+    </button>
+  </MenuItem>
+);
+
+// TODO: Extract
+const Icon = () => (
+  <svg className="icon icon--checkmark" height="18" viewBox="2 2 22 22" width="18" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z" />
+  </svg>
+);
+
+export default SelectableMenuItem;
